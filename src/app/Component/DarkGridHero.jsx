f5a3a7230480e80b1fd81fe36606d34a1b39d424
Meghna Varma <114867153+meghnavarma25@users.noreply.github.com>
2025-06-05 17:09:32 +0530

    Update DarkGridHero.jsx

--- src/app/Component/DarkGridHero.jsx
+++ src/app/Component/DarkGridHero.jsx
@@ -13,7 +13,7 @@ export const DarkGridHero = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await fetch("http://15.207.55.161/classify", {
+      const result = await fetch("http://43.204.234.191/classify", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
